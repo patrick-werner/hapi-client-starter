d4e8663eaab52876ae31d936e61bf3364067ca0d
patrick-werner <pa.f.werner@gmail.com>
2021-06-03 12:02:42 +0200

    implemented Patient

--- src/main/java/HapiClientStarter.java
+++ src/main/java/HapiClientStarter.java
@@ -1,21 +1,15 @@
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
-import org.hl7.fhir.r4.model.Address;
-import org.hl7.fhir.r4.model.BooleanType;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Bundle.HTTPVerb;
-import org.hl7.fhir.r4.model.DateTimeType;
-import org.hl7.fhir.r4.model.IdType;
-import org.hl7.fhir.r4.model.Observation;
-import org.hl7.fhir.r4.model.Observation.ObservationComponentComponent;
-import org.hl7.fhir.r4.model.Observation.ObservationStatus;
-import org.hl7.fhir.r4.model.OperationOutcome;
+import java.util.Date;
+import org.hl7.fhir.r4.model.DateType;
+import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
+import org.hl7.fhir.r4.model.HumanName;
+import org.hl7.fhir.r4.model.HumanName.NameUse;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Patient;
-import org.hl7.fhir.r4.model.Practitioner;
-import org.hl7.fhir.r4.model.Quantity;
-import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.StringType;
 
 public class HapiClientStarter {
 
@@ -24,5 +18,32 @@ public class HapiClientStarter {
     String serverBase = "http://hapi.fhir.org/baseR4";
     IGenericClient client = ctx.newRestfulGenericClient(serverBase);
     client.registerInterceptor(new LoggingInterceptor());
+
+    Patient patient = new Patient();
+
+    patient.setId("123456");
+
+    patient.setActive(true);
+    patient.setGender(AdministrativeGender.FEMALE);
+    patient.setBirthDateElement(new DateType("1980-05-03"));
+    // identisch: patient.setBirthDate(new Date("1980-05-03"));
+
+    Identifier identifier = patient.addIdentifier();
+    identifier.getType().addCoding().setSystem("http://terminology.hl7.org/CodeSystem/v2-0203")
+        .setCode("MR");
+    identifier.setSystem("http://exampleHospital.com/fhir/sid/Patientennummer")
+        .setValue("123456789");
+
+    HumanName name = patient.addName();
+    name.setUse(NameUse.OFFICIAL);
+    name.setFamily("Nachname");
+    name.getFamilyElement().addExtension()
+        .setUrl("http://hl7.org/fhir/StructureDefinition/humanname-own-name")
+        .setValue(new StringType("Nachname"));
+    name.addGiven("Vorname");
+    name.setText("Vorname Nachname");
+
+    IParser jsonParser = ctx.newJsonParser().setPrettyPrint(true);
+    System.out.println(jsonParser.encodeResourceToString(patient));
   }
 }
