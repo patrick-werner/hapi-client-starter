ae983e8520ce0d8b886454a32be977b48ed99610
patrick-werner <pa.f.werner@gmail.com>
2022-04-04 08:00:41 +0200

    start

---
+++ src/main/java/client_test/HapiClientStarter.java
@@ -0,0 +1,30 @@
+package client_test;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import java.util.Date;
+import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.DateType;
+import org.hl7.fhir.r4.model.Encounter;
+import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
+import org.hl7.fhir.r4.model.HumanName.NameUse;
+import org.hl7.fhir.r4.model.Identifier;
+import org.hl7.fhir.r4.model.OperationOutcome;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Reference;
+
+public class HapiClientStarter {
+
+  public static void main(String[] args) {
+    FhirContext ctx = FhirContext.forR4();
+    String serverBase = "http://hapi.fhir.org/baseR4";
+    IGenericClient client = ctx.newRestfulGenericClient(serverBase);
+
+    IParser parser = ctx.newJsonParser().setPrettyPrint(true);
+
+   // insert your code here
+
+  }
+}
