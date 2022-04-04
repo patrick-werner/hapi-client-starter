29c50f2d5813ad9c5467657eb4cb5e7b73da6d4a
patrick-werner <pa.f.werner@gmail.com>
2022-04-04 12:59:13 +0200

    pojos ready

--- src/main/java/client_test/HapiClientStarter.java
+++ src/main/java/client_test/HapiClientStarter.java
@@ -5,9 +5,15 @@ import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import java.util.Date;
 import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.Encounter;
 import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
+import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.HumanName.NameUse;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Reference;
 
 public class HapiClientStarter {
 
@@ -21,6 +27,9 @@ public class HapiClientStarter {
 
     Patient pat = createPatient();
     System.out.println(parser.encodeResourceToString(pat));
+    Encounter enc = createEncounter(pat);
+    System.out.println(parser.encodeResourceToString(enc));
+    Condition cond = createCondition(pat, enc);
   }
 
   private static Patient createPatient() {
@@ -38,5 +47,80 @@ public class HapiClientStarter {
     patient.setGender(AdministrativeGender.OTHER);
     patient.setBirthDate(new Date());
     return patient;
+  }
+
+  private static Condition createCondition(Patient patient, Encounter encounter) {
+    Condition condition = new Condition();
+    condition.setRecordedDate(new Date());
+    condition
+        .getClinicalStatus()
+        .addCoding()
+        .setSystem("http://terminology.hl7.org/CodeSystem/condition-clinical")
+        .setCode("active");
+    condition.setSubject(new Reference(patient));
+    condition.setEncounter(new Reference(encounter));
+    condition
+        .getCode()
+        .addCoding()
+        .setSystem("http://snomed.info/sct")
+        .setCode("389145006")
+        .setDisplay("allergisches Asthma");
+    return condition;
+  }
+
+  private static Encounter createEncounter(Patient patient) {
+    Encounter enc = new Encounter();
+    enc.setId("encounter01");
+    Identifier identifier = enc.addIdentifier();
+    identifier
+        .getType()
+        .addCoding()
+        .setSystem("http://terminology.hl7.org/CodeSystem/v2-0203")
+        .setCode("VN");
+    identifier.setSystem("http://meinkrankenhaus.de/sid/Fallnummer").setValue("0815");
+    enc.getServiceType()
+        .addCoding()
+        .setSystem("http://fhir.de/CodeSystem/dkgev/Fachabteilungsschluessel")
+        .setCode("0300")
+        .setDisplay("Kardiologie");
+    enc.getPeriod().setStart(new Date());
+    enc.setSubject(new Reference(patient));
+    enc.getClass_()
+        .setSystem("http://terminology.hl7.org/CodeSystem/v3-ActCode")
+        .setCode("IMP")
+        .setDisplay("inpatient encounter");
+    enc.getHospitalization()
+        .getAdmitSource()
+        .addCoding()
+        .setSystem("http://fhir.de/CodeSystem/dgkev/Aufnahmeanlass")
+        .setCode("N")
+        .setDisplay("Notfall");
+    Extension extension = enc.addExtension();
+    extension.setUrl("http://fhir.de/StructureDefinition/Aufnahmegrund");
+    extension
+        .addExtension()
+        .setUrl("ErsteUndZweiteStelle")
+        .setValue(
+            new Coding()
+                .setSystem("http://fhir.de/CodeSystem/dkgev/AufnahmegrundErsteUndZweiteStelle")
+                .setCode("01")
+                .setDisplay("Krankenhausbehandlung, vollstationär"));
+    extension
+        .addExtension()
+        .setUrl("DritteStelle")
+        .setValue(
+            new Coding()
+                .setSystem("http://fhir.de/CodeSystem/dkgev/AufnahmegrundDritteStelle")
+                .setCode("0"));
+    extension
+        .addExtension()
+        .setUrl("VierteStelle")
+        .setValue(
+            new Coding()
+                .setSystem("http://fhir.de/CodeSystem/dkgev/AufnahmegrundVierteStelle")
+                .setCode("1")
+                .setDisplay("Normalfall"));
+
+    return enc;
   }
 }
