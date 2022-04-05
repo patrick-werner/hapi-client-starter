f1a0b1dbc94e6d7f6c316109431a4138b86c179c
patrick-werner <pa.f.werner@gmail.com>
2022-04-05 10:08:03 +0200

    fuer fhirpathUbebung

--- src/main/java/client_test/FhirPathTester.java
+++ src/main/java/client_test/FhirPathTester.java
@@ -2,7 +2,13 @@ package client_test;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
+import java.util.List;
+import org.hl7.fhir.r4.hapi.fluentpath.FhirPathR4;
+import org.hl7.fhir.r4.model.Base;
+import org.hl7.fhir.r4.model.Extension;
+import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Type;
 
 public class FhirPathTester {
   public static void main(String[] args) {
@@ -10,5 +16,17 @@ public class FhirPathTester {
     IParser iParser = ctx.newJsonParser().setPrettyPrint(true);
 
     Patient patient = HapiClientStarter.createPatient();
+    FhirPathR4 fhirpath = new FhirPathR4(ctx);
+    List<Base> evaluate = fhirpath.evaluate(patient, "Patient.name", Base.class);
+    System.out.println(evaluate.size());
+    evaluate.forEach(
+        e -> {
+          Extension ext = new Extension();
+          ext.setUrl("http://example.org");
+          ext.setValue((Type) e);
+          Observation obs = new Observation();
+          obs.addExtension(ext);
+          System.out.println(iParser.encodeResourceToString(obs));
+        });
   }
 }
